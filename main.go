aeb3e0a45305a6189a0fc5178d8dffa70eaf13d1
José Borges <jose.carlos05062000@gmail.com>
2025-08-11 21:26:48 -0300

    Modificações de saídas e execução da função Adivinhacao na func main

--- main.go
+++ main.go
@@ -12,17 +12,18 @@ func Adivinhacao() {
 
 	for {
 		var palpite int
-		fmt.Print("Digite um número (1-100):")
+		fmt.Print("Digite um número (1-100): ")
 		fmt.Scan(&palpite)
 
 		if palpite == alvo {
-			fmt.Printf("Você acertou! o número secreto era: %v", alvo)
+			fmt.Println("Você acertou! o número secreto era: ", alvo)
+			fmt.Println("Número de tentativas:", tentativas+1)
 			os.Exit(-1)
 		} else {
 			if palpite < alvo {
-				fmt.Printf("O número é maior!")
+				fmt.Println("O número é maior!")
 			} else if palpite > alvo {
-				fmt.Printf("O número é menor!")
+				fmt.Println("O número é menor!")
 			}
 		}
 		tentativas++
@@ -30,5 +31,5 @@ func Adivinhacao() {
 }
 
 func main() {
-
+	Adivinhacao()
 }
