32e388097c985e129275e09531e4b1fedff4ecd6
José Borges <jose.carlos05062000@gmail.com>
2025-08-11 21:19:41 -0300

    Criada a função para o jogo

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"fmt"
+	"math/rand/v2"
+	"os"
+)
+
+func Adivinhacao() {
+	alvo := rand.IntN(100) + 1
+	tentativas := 0
+
+	for {
+		var palpite int
+		fmt.Print("Digite um número (1-100):")
+		fmt.Scan(&palpite)
+
+		if palpite == alvo {
+			fmt.Printf("Você acertou! o número secreto era: %v", alvo)
+			os.Exit(-1)
+		} else {
+			if palpite < alvo {
+				fmt.Printf("O número é maior!")
+			} else if palpite > alvo {
+				fmt.Printf("O número é menor!")
+			}
+		}
+		tentativas++
+	}
+}
+
 func main() {
 
 }
